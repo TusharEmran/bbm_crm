ea2ef0d962787ca6c15f73ca9e91f6fbc8b8e9eb
TusharEmran <124170011+TusharEmran@users.noreply.github.com>
2025-11-08 15:24:45 +0600

    added options

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    optionsSuccessStatus: 200
+  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
 }
 
 app.use(cors(corsOptions));
